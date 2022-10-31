f1153d7f5c0be87774c333908ed7225f67391857
Black Dzen <blackdzendev@gmail.com>
2022-10-31 22:08:19 +0300

    Configured ./backend/App.js. Solved bug with incorrect responses from backend App.js

--- backend/App.js
+++ backend/App.js
@@ -1,53 +1,54 @@
-require('dotenv').config()
-const cors = require('cors')
-const { json } = require('express')
-const PORT = process.env.PORT
+require("dotenv").config();
+const cors = require("cors");
+const { json } = require("express");
+const SERVER_PORT = process.env.SERVER_PORT;
 
-const express = require('express')
-const app = express()
-app.use(express.json())
-app.use(cors())
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const Contact = require('./models/contact')
+const Contact = require("./models/contact");
 
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT} port`)
-})
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is running on ${SERVER_PORT} port`);
+});
 
-app.get('/contacts', (request, response) => {
-  Contact.find({}).then((contacts) => response.json(contacts))
-})
+app.get("/contacts", (request, response) => {
+  Contact.find({}).then((contacts) => response.json(contacts));
+});
 
-app.post('/contacts', (request, response) => {
-  let contact = new Contact(request.body)
-  contact.save()
-  response.sendStatus(201).end()
-  return response.json()
-})
+app.post("/contacts", (request, response) => {
+  let contact = new Contact(request.body);
+  contact.save().then(() => {
+    response.status(200)
+    return response.json({ answer: 'contact added' });
+  });
+});
 
-app.delete('/contacts/:id', (request, response) => {
+app.delete("/contacts/:id", (request, response) => {
   Contact.findByIdAndRemove(request.params.id)
-    .then((response) => {
-      response.sendStatus(204).send()
+    .then(() => {
+      response.status(204)
+      return response.json({ answer: 'contact deleted' });
     })
-    .catch(() => {
-      console.log('Some error')
-    })
-  return response.json()
-})
-app.put('/contacts/:id', (request, response) => {
-  const body = request.body
-  console.log(body)
-  console.log(request.params.id)
+    .catch((error) => {
+      console.log("Some error:" + error);
+    });
+});
+app.put("/contacts/:id", (request, response) => {
+  const body = request.body;
+  console.log(body);
+  console.log(request.params.id);
   const editedContact = {
     firstName: body.firstName,
     lastName: body.lastName,
-    phoneNumber: body.phoneNumber
-  }
+    phoneNumber: body.phoneNumber,
+  };
   Contact.findByIdAndUpdate(request.params.id, editedContact, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote)
+    .then((updatedNote) => {
+      response.status(200)
+      response.json({ answer: 'contact have been changed' });
     })
-    .catch(error => console.log(error))
-  return response.json()
-})
+    .catch((error) => console.log(error));
+});
